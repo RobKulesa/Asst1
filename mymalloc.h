f18f4eedc8856daad76951868e561fdcd7415443
Rob Kulesa <robjkulesa@gmail.com>
2020-10-21 22:21:04 -0400

    10/21
    Moved function prototypes to bottom in mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -4,12 +4,11 @@
 #define DEBUG 0
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
-
-void *mymalloc(size_t length, char *file, int line);
-void myfree(void *ptr, char *file, int line);
-Metadata *incrementPointer(Metadata *ptr, int n);
-
 typedef struct Metadata {
     int status;
     int length;
 } Metadata;
+
+void *mymalloc(size_t length, char *file, int line);
+void myfree(void *ptr, char *file, int line);
+Metadata *incrementPointer(Metadata *ptr, int n);
